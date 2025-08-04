9f4c7872027e5d2261471226b665de4072f925d3
kumar.krish07022002@gmail.com <kumar.krish07022003@gmail.com>
2025-08-04 17:17:12 +0530

    ##

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/socketContext.jsx'
+import { SocketContextProvider } from './context/SocketContext.jsx'
 
 
 ReactDom.createRoot(document.getElementById('root')).render(
